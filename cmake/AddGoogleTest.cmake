33d139384c62e8b7835177426f29b075f4d81c6f
Zachary Selk <zrselk@gmail.com>
2023-09-09 13:29:12 -0600

    Added debug to workflow

--- cmake/AddGoogleTest.cmake
+++ cmake/AddGoogleTest.cmake
@@ -65,21 +65,8 @@ endif()
 macro(add_gtest TESTNAME)
     target_link_libraries(${TESTNAME} PUBLIC gtest gmock gtest_main)
 
-    if(GOOGLE_TEST_INDIVIDUAL)
-        if(CMAKE_VERSION VERSION_LESS 3.10)
-            gtest_add_tests(TARGET ${TESTNAME}
-                            TEST_PREFIX "${TESTNAME}."
-                            TEST_LIST TmpTestList)
-            set_tests_properties(${TmpTestList} PROPERTIES FOLDER "Tests")
-        else()
-            gtest_discover_tests(${TESTNAME}
-                TEST_PREFIX "${TESTNAME}."
-                PROPERTIES FOLDER "Tests")
-        endif()
-    else()
-        add_test(${TESTNAME} ${TESTNAME})
-        set_target_properties(${TESTNAME} PROPERTIES FOLDER "Tests")
-    endif()
+    add_test(${TESTNAME} ${TESTNAME} WORKING_DIRECTORY ${BINARY_INSTALL_DIR}/Debug)
+    #set_target_properties(${TESTNAME} PROPERTIES FOLDER "Tests")
 
 endmacro()
 
